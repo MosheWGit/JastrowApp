696d64328955550816d728774c9606cb825650ad
Noah Sragow <nsragow@gmail.com>
2018-01-14 20:24:11 -0500

    further set up, just removed all flying

--- app/src/main/java/com/example/moish/jastrowapp/MainActivity.java
+++ app/src/main/java/com/example/moish/jastrowapp/MainActivity.java
@@ -4,17 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 
 import com.flyingpenguins.app.*;
-
-import static android.os.Environment.getExternalStorageDirectory;
 
 public class MainActivity extends AppCompatActivity {
 
